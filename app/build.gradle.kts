e1086acaf15dd6a3c2957031fbd94a71a3db63f5
Jaehong Yoo <rouddy@naver.com>
2024-01-28 21:23:19 +0900

    compile, target version change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.algorigo.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.algorigo.myapplication"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
